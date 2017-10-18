46ef5afa170837e4e5e52d352895c7b55b93c079
Petar Gitnik <petarGitNik@users.noreply.github.com>
2017-10-18 18:22:38 +0200

    Regulate timer using pomodoroClock class

--- js/main.js
+++ js/main.js
@@ -1,33 +1,84 @@
-function padding(time) {
-  if (time.toString().length === 1) {
-    return '0' + time;
-  } else {
-    return time;
+class pomodoroClock {
+  constructor (seconds, timeString) {
+    this._timeInSeconds = seconds;
+    this._timeString = timeString;
   }
-}
 
+  get timeInSeconds() {
+    return this._timeInSeconds;
+  }
 
-function prettifyTimer(minutes, seconds) {
-  return `${padding(minutes)}:${padding(seconds)}`;
+  get timeString() {
+    return this._timeString;
+  }
+
+  changeTime(seconds) {
+    // seconds -> e.g. 1470 seconds
+    // timerSeconds -> remainder for the clock 30 seconds
+    this._timeInSeconds = seconds;
+
+    let hours = Math.floor(seconds / 3600);
+    let remainder = seconds % 3600;
+
+    let minutes = Math.floor(remainder / 60);
+    let timerSeconds = Math.floor(remainder % 60);
+
+    this._timeString = pomodoroClock.prettifyTimer(minutes, timerSeconds);
+  }
+
+  static padding(time) {
+    if (time.toString().length === 1) {
+      return '0' + time;
+    } else {
+      return time;
+    }
+  }
+
+  static prettifyTimer(minutes, seconds) {
+    return `${pomodoroClock.padding(minutes)}:${pomodoroClock.padding(seconds)}`;
+  }
+
 }
 
 
 function changeTimer(time) {
+  /* Change the timer on the webpage */
   let hours = Math.floor(time / 3600);
   let remainder = time % 3600;
 
   let minutes = Math.floor(remainder / 60);
   let seconds = Math.floor(remainder % 60);
 
-  document.getElementById('timer').innerHTML = prettifyTimer(minutes, seconds);
+  document.getElementById('timer').innerHTML = pomodoroClock.prettifyTimer(minutes, seconds);
 }
 
 
+function decrementTime(timeObject) {
+  /* Decrement internal time of the pomodoroClock object */
+  let currentTime = timeObject.timeInSeconds;
+  if (currentTime > 0) {
+    timeObject.changeTime(currentTime - 1);
+    changeTimer(timeObject.timeInSeconds);
+  }
+}
+
+
+function changeAndDecrement(timeObject) {
+  /* Decrement the time of the pomodoroClock object, and change the display */
+  decrementTime(timeObject);
+  changeTimer(timeObject.timeInSeconds);
+}
+
+
+// Set event listeners when the page loads
 function onLoad() {
+
+  let timerOn = false;
+  let pomodoro = new pomodoroClock(25, '25:00');
 
   // Incremment session time
   document.getElementById('up-session').addEventListener('click', function() {
-    let timeInMinutes = Number(document.getElementById('time-session').innerHTML ) + 1;
+    let timeInMinutes = Number(document.getElementById('time-session').innerHTML) + 1;
     document.getElementById('time-session').innerHTML = timeInMinutes;
     changeTimer(timeInMinutes * 60);
   });
@@ -42,7 +93,7 @@ function onLoad() {
   document.getElementById('down-session').addEventListener('click', function() {
     let sessionTime = document.getElementById('time-session').innerHTML;
     if (sessionTime > 0) {
-      let timeInMinutes = Number(document.getElementById('time-session').innerHTML ) - 1;
+      let timeInMinutes = Number(document.getElementById('time-session').innerHTML) - 1;
       document.getElementById('time-session').innerHTML = timeInMinutes;
       changeTimer(timeInMinutes * 60)
     } else {
@@ -62,7 +113,13 @@ function onLoad() {
 
   // Start the clock
   document.getElementById('start').addEventListener('click', function() {
+    if (!timerOn) {
+      timerOn = true;
 
+      let time = Number(document.getElementById('time-session').innerHTML) * 60;
+      pomodoro.changeTime(time);
+      setInterval(changeAndDecrement, 1000, pomodoro);
+    }
   });
 
 }
